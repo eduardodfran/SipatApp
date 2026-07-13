532dac36e2af4f9ed73bb52035a0023f505c3070
eduardo <106855879+eduardodfran@users.noreply.github.com>
2026-07-13 20:19:10 +0800

    feat: adjust padding for header in DashboardScreen based on platform

--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -354,7 +354,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingTop: 20,
+    paddingTop: Platform.OS === 'ios' ? 54 : 20,
     paddingBottom: 12,
     paddingHorizontal: 20,
   },
